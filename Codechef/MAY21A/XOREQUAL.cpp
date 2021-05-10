1d1eb03f6e20f53eb828b2fd5bc8dca64a0b135c
Pulkit Singhvi <37381223+singhvipulkit112@users.noreply.github.com>
2021-05-10 12:04:27 +0530

    Solution : Xor Equality
    Link : https://www.codechef.com/MAY21A/problems/XOREQUAL/
    Tags : Maths/ Bit manipulation

---
+++ Codechef/MAY21A/XOREQUAL.cpp
@@ -0,0 +1,32 @@
+// AUTHOR -- PULKIT SINGHVI @ps112iitkgp
+#include <bits/stdc++.h>
+using namespace std;
+
+#define test() int t;cin>>t;while(t--) 
+const int mod = (int)1e9+7;
+
+int mul(int a, int b)
+{
+    return((a*1LL*b)%mod);
+}
+
+int power(int x, int a)
+{
+    if(a==0)return(1);
+    int z = power(x, a/2);
+    z=mul(z, z);
+    if(a%2)z=mul(z, x);
+    return(z);
+}
+
+int main()
+{
+    int mod_inv_4 = power(4, mod-2);
+	  test()
+    {
+        int n;cin>>n;
+        int ans = mul(power(2, n+1), mod_inv_4);
+
+        cout<<ans<<'\n';
+    }
+}   
